260b9321bff97ebf53e120f4181d3ba5e1241721
daviderocco85 <roccodavide85@gmail.com>
2026-09-02 13:27:29 +0200

    feat: register posts router in app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { posts } from './routers/posts.js'
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/posts', posts);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
